5a0538fa1c96b896b3b491441c0b05c375d2efee
Chrahuldeveloper <chrahulofficial@gmail.com>
2025-09-26 03:22:31 +0530

    fixed

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ TerminalInput.addEventListener("keypress", (e) => {
         terminalBox.innerHTML += `
     <div class="bg-black text-gray-200 font-mono p-4">
       <h1 class="text-green-400 font-semibold">$ help</h1>
-      <p class="mt-2">Available commands:</p>
-      <pre id="helpTyped" class="mt-2 text-xs sm:text-base"></pre>
-      <p class="mt-4">Special commands:</p>
-      <pre id="specialTyped" class="mt-2 text-sm sm:text-base"></pre>
+      <p class="mt-2 text-green-400">Available commands:</p>
+      <pre id="helpTyped" class="mt-2 text-xs sm:text-base text-green-400"></pre>
+      <p class="mt-4 text-green-400">Special commands:</p>
+      <pre id="specialTyped" class=" text-green-400 mt-2 text-sm sm:text-base"></pre>
     </div>
   `;
 
@@ -48,9 +48,9 @@ clear          - Clear terminal output`,
         terminalBox.innerHTML += `
           <div class="bg-black text-gray-200 font-mono p-4 space-y-3.5">
             <h1 class="text-green-400 font-semibold">$ about</h1>
-            <h2 class="text-white font-bold">About Rahul</h2>
-            <p>==================</p>
-            <p>
+            <h2 class="text-green-400 font-bold">About Rahul</h2>
+            <p class="text-green-400">==================</p>
+            <p class="text-green-400">
               I'm a passionate coder with 5+ years of experience building web applications and SaaS products that users love
               When I'm not coding, you'll find me contributing to open source projects, writing technical blogs, or exploring the latest in Tech             
               <strong>Location:</strong> Hyderabad, IN<br>
@@ -64,7 +64,7 @@ clear          - Clear terminal output`,
         terminalBox.innerHTML += `
   <div class="bg-black text-green-400 font-mono p-4 my-5">
     <h1 class="text-green-400 font-semibold">$ skills</h1>
-    <h2 class="mt-2 text-lg">Technical Skills</h2>
+    <h2 class="mt-2 text-lg text-green-400">Technical Skills</h2>
     <p class="text-green-500">________________________</p>
 
     <!-- Programming -->
@@ -178,32 +178,32 @@ clear          - Clear terminal output`,
         terminalBox.innerHTML += `
       <div class="bg-black text-slate-200 font-mono p-4">
         <h1 class="text-green-500 font-semibold">$ projects</h1>
-        <h2 class="mt-2 text-lg">Featured Projects</h2>
-        <p class="text-slate-200">==================</p>
+        <h2 class="mt-2 text-lg text-green-400">Featured Projects</h2>
+        <p class="text-green-400">==================</p>
 
         <!-- Project 1 -->
-        <div class="mt-4">
+        <div class="mt-4 text-green-400">
           <p>[1] <span class="font-bold">FeedSenseAI</span></p>
           <p class="pl-6 text-sm">An AI-powered feedback management platform that converts user feedback into actionable insights. Businesses can integrate a customizable feedback form on their websites, and FeedSenseAI analyzes the data to suggest improvements, helping teams make data-driven decisions without guesswork</p>
           <p class="pl-6">→ <a href="https://feedsenseai.vercel.app/" target="_blank" class="text-blue-500 hover:underline">Link</a></p>
         </div>
 
         <!-- Project 2 -->
-        <div class="mt-4">
+        <div class="mt-4 text-green-400">
           <p>[2] <span class="font-bold">Matrix</span></p>
           <p class="pl-6 text-sm">Matrix is a networking app for tech enthusiasts across various domains. With 65 active users, it helps you discover and connect with like-minded people, chat with them, and share updates about your projects. Whether you’re looking to collaborate, learn, or showcase your work, Matrix makes networking simple and meaningful.</p>
           <p class="pl-6">→ <a href="https://matrixappcom.netlify.app/" target="_blank" class="text-blue-500 hover:underline">Link</a></p>
         </div>
 
         <!-- Project 3 -->
-        <div class="mt-4">
+        <div class="mt-4 text-green-400">
           <p>[3] <span class="font-bold">InterviewPrepAI</span></p>
           <p class="pl-6 text-sm">An AI-driven interview preparation platform that generates tailored interview questions based on a candidate’s profile or job description. It helps users practice effectively, identify knowledge gaps, and boost confidence for real interviews.</p>
           <p class="pl-6">→ <a href="https://chromewebstore.google.com/detail/interviewprepai/jfpbdijbapmdgnimkpdhmolmefjhbccp" target="_blank" class="text-blue-500 hover:underline">Link</a></p>
         </div>
 
         <!-- Project 4 -->
-        <div class="mt-4">
+        <div class="mt-4 text-green-400">
           <p>[4] <span class="font-bold">TapNFill</span></p>
           <p class="pl-6 text-sm">A seamless productivity tool that simplifies repetitive workflows. Users can automate filling forms and repetitive tasks, saving time and reducing errors, making everyday digital interactions faster and smarter.</p>
           <p class="pl-6">→ <a href="https://chromewebstore.google.com/detail/tapnfill/mgenpnofgolhidfgggfhicbigpjmnokn" target="_blank" class="text-blue-500 hover:underline">Link</a></p>
