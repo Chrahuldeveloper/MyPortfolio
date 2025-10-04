8e4a3ca672df05ed79fa43509e1507ebd7534355
Chrahuldeveloper <chrahulofficial@gmail.com>
2025-10-04 22:15:13 +0530

    fixed

--- server/src/routes/SaveDetails.ts
+++ server/src/routes/SaveDetails.ts
@@ -14,11 +14,24 @@ saveDetails.post("/api/user", async (req, res) => {
     }
 
     const { name, email } = req.body;
+
     if (!name || !email) {
       return res.status(404).json({
         err: "Name and email are required",
       });
     }
+
+    const existingEmails = await db.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (existingEmails.rows[0]) {
+      return res.status(429).json({
+        error: "Email already exits",
+      });
+    }
+
     await db.query("INSERT INTO users (name,email) VALUES  ($1,$2)", [
       name,
       email,
